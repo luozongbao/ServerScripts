b2024b081104490520be77241b2a414e60ba7759
Atipat Lorwongam <atipat.lorwongam@outlook.com>
2024-06-21 20:40:00 +0700

    add restorewp.sh

---
+++ restoresp.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# This checks if the number of arguments is correct
+# If the number of arguments is incorrect ( $# != 2) print error message and exit
+if [[ $# != 4 ]]
+then
+  echo "restorewp.sh [archive_file] [destination] [DBUser] [DBPassword]"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -e $1 ]] 
+then
+  echo "Invalid archive file path provided"
+  exit
+fi
+
+# This checks if argument 1 and argument 2 are valid directory paths
+if [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+fullArchiveFile=$1
+destinationPath=$2
+DBUser=$3
+DBPass=$4
+originPath=$(pwd)
+archiveFileName=$(basename $fullArchiveFile)
+archivePath=$(dirname $fullArchiveFile)
+originalFolder=$(basename $(echo $archiveFileName | cut -d "-" -f2) .zip)
+WPCONFIG="$archivePath/$originalFolder/wp-config.php"
+cd $destinationPath
+destinationPath=$(pwd)
+cd $originPath
+
+echo "Extracting file $archiveFileName"
+cd $archivePath
+unzip -o -q  $archiveFileName
+
+if [[ ! $? == 0 ]]
+then
+  echo "Error occor while extracting file"
+  exit
+fi
+
+if [[ ! -e $WPCONFIG ]]
+then
+  echo "Could not find wp-config.php"
+  exit
+fi
+
+DBName=$(cat $WPCONFIG | grep DB_NAME | cut -d \' -f 4)
+
+echo "Creating database $DBName"
+mysql -u root -e "CREATE DATABASE $DBName;"
+if [[ ! $? == 0 ]]
+then
+  echo "Error occor while creating database"
+  exit
+fi
+
+echo "Creating database user: $DBUser"
+mysql -u root -e "CREATE USER $DBUser IDENTIFIED BY '$DBPass';"
+
+if [[ ! $? == 0 ]]
+then
+  echo "Error occor while creating database user"
+  exit
+fi
+
+echo "Importing Database"
+mysql -u $DBUser --password="$DBPass" $DBName < ${DBName}.sql
+if [[ ! $? == 0 ]]
+then
+  echo "Error occor while importing database $DBName"
+  exit
+fi
+
+echo "removing $DBName.sql and moving $originalFolder to $destinationPath"
+rm ${DBName}.sql
+mv $originalFolder $destinationPath
+
+
+
+
+
+
+
+
+
