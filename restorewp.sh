ab36de29b956539d35c5ffeb74d77580af5b9df9
Atipat Lorwongam <atipat.lorwongam@outlook.com>
2024-07-01 01:59:54 +0700

    update restorewp.sh

--- restorewp.sh
+++ restorewp.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Check if run as root
+if (( $EUID != 0 )) 
+then
+	echo "Please run as root"
+	exit
+fi
+
+# Check if mysql is installed
+if [[ -z $(command -v mysql) ]]
+then
+  echo "mysql is not installed"
+  exit
+fi
+
 # This checks if the number of arguments is correct
 # If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ !  ($# == 2 || $# == 4) ]]
+if [[ ( $# < 2 || $# > 5 ) ]]
 then
-  echo -e "Using original database username and password: restorewp.sh [archive_file] [destination] \ndefine new database username and password: restorewp.sh [archive_file] [destination] [dbuser] [dbpassword]"
+  echo -e "restorewp.sh [archive_file] [destination]"
+  echo -e "restorewp.sh [archive_file] [destination] [newsiteurl]"
+  echo -e "restorewp.sh [archive_file] [destination] [dbuser] [dbpassword]"
+  echo -e "restorewp.sh [archive_file] [destination] [dbuser] [dbpassword] [newsiteurl]"
   exit
 fi
 
@@ -55,26 +72,32 @@ wordpressFolder=$(dirname $WPCONFIG | rev | cut -d "/" -f 1 | rev)
 
 mv $wordpressFolder $destinationPath
 echo "$wordpressFolder moved to $destinationPath"
+WPDIR="$destinationPath/$wordpressFolder/"
 WPCONFIG="$destinationPath/$wordpressFolder/wp-config.php"
+
 
 # acquire databasename
 DBName=$(cat $WPCONFIG | grep DB_NAME | cut -d \' -f 4)
 
-if [[ $# == 2 ]]
+if [[ ($# == 2 || $# == 3) ]]
 then
   # acquire database username
   DBUser=$(cat $WPCONFIG | grep DB_USER | cut -d \' -f 4)
-else
+elif [[ ($# == 4 || $# == 5) ]]
+then
   DBUser=$3
 fi
 
-if [[ $# == 2 ]]
+if [[ ($# == 2 || $# == 3) ]]
 then
   # acquire database user password
   DBPass=$(cat $WPCONFIG | grep DB_PASSWORD | cut -d \' -f 4)
-else
+elif [[ ($# == 4 || $# == 5) ]]
+then
   DBPass=$4
 fi
+# acquire database Prefix
+DBPREFIX=$(cat $WPCONFIG | grep "\$table_prefix" | cut -d \' -f 2)
 
 # Create database
 echo "Creating database $DBName"
@@ -137,6 +160,35 @@ fi
 
 echo "removing $DBName.sql"
 rm ${DBName}.sql
+
+# Configure Site URL
+# Get current site URL
+#originalURL=$(sudo wp option get siteurl --path=$WPDIR --allow-root)
+QUERY="SELECT option_value FROM ${DBPREFIX}options WHERE option_id=1;"
+originalURL=$(mysql -u $DBUser $DBName -p$DBPass -e "$QUERY")
+originalURL=$(echo $originalURL | grep -oP '\s(.*)$')
+echo "current site url is: $originalURL"
+
+if [[ ! -z $(ls /usr/local/bin/ | grep -E "^wp$") ]]
+then
+  if [[ ($# == 3 || $# == 5) ]]
+  then
+    if [[ ($# == 3) ]]; then newURL=$3; fi
+    if [[ ($# == 5) ]]; then newURL=$5; fi
+    wp search-replace $originalURL $newURL --path=$WPDIR --all-tables --allow-root
+    if [[ ! $? == 0 ]]
+    then
+      echo "Error occor while configuring site url"
+      exit
+    else 
+      echo "site url changed to $newURL"
+    fi
+
+  fi
+fi
+
+
+
 
 
 
